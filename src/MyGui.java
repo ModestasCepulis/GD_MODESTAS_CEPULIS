31cbc49745b99bdc4452e3d0e681ffce9c005138
ModestasCepulis <modestas.cepulis@students.ittralee.ie>
2018-11-06 15:51:06 +0000

    Trying to do different panels showing up when different buttons are pressed.
    Will try to do that different GUI'S are made when the button is pressed instead of having different panels showing up.
    
    For now managed to do 2 different panels.
    
    Trying to tidy the code up by putting everything in different user-defined methods.
    
    Next aim is to tidy up the overall look of the panels, make the buttons look more better.
    
    Later on I will try to add different buttons at the start, eg: 'Options', 'Exit'.
    
    Modestas Cepulis t00199237
    06/11/2018

--- src/MyGui.java
+++ src/MyGui.java
@@ -2,62 +2,68 @@ import org.w3c.dom.Text;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //JFrame extends MyGui which means that we can use the public methods that are in JFrame class
 public class MyGui extends JFrame {
 
-
+    JPanel launchGamePanel = new JPanel();
+    JPanel firstScenePanel = new JPanel();
+    
     public static void main(String[] args) {
-
         new MyGui();
-
     }
 
     public MyGui()
     {
-
-
-
-
-
-
-        //---------First Launch Panel------------
-
-        //Creates a text area
-        JTextArea textArea = new JTextArea();
+        //=================LAUNCH TEXT AREA ======================
+        //Creates a button
+        JButton launchSceneButton = new JButton("Start");
+        JTextArea launchTextarea = new JTextArea();
         //Sets the text area text to be
-        textArea.setText("\n\n\nBegin your Adventure!");
+        launchTextarea.setText("\n\n\n\tBegin your Adventure!");
         //Creates a Font object which sets the font parameters
-        Font font = new Font("Segoe Script", Font.BOLD, 50);
+        Font font  = new Font("Tahoma", Font.BOLD, 50);
         //Sets the text area font to be equal to our Font object
-        textArea.setFont(font);
+        launchTextarea.setFont(font);
         //Block the user from editing the textArea
-        textArea.setEditable(false);
-        textArea.setForeground(Color.white);
-        textArea.setBackground(Color.DARK_GRAY);
+        launchTextarea.setEditable(false);
+        launchTextarea.setForeground(Color.white);
+        launchTextarea.setBackground(Color.DARK_GRAY);
+        //=================LAUNCH TEXT AREA END ======================
 
-        /*
-        //Creates a label area (Like a title)
-        JLabel label = new JLabel();
-        label.setText("Begin your Adventure!");
-        */
 
-        //This creates a panel, (More like a holder) for the objects that you
-        //want to keep in this specific container and which ones you want to show/add
-        JPanel panel = new JPanel();
-        panel.add(textArea);
-        //Adds specified objects to the panel
-        /*panel.add(label);*/
+        //=================FIRST SCENE TEXT AREA ======================
+        //Creates a button
+        JButton firstChoiceButton = new JButton("Up");
+        JButton secondChoiceButton = new JButton("Left");
+        JButton thirdChoiceButton = new JButton("Right");
+        JTextArea firstTextArea = new JTextArea();
+        //Sets the text area text to be
+        firstTextArea.setText("\n\n\n\tWhere would you want to go?");
+        //Creates a Font object which sets the font parameters
+        Font font2  = new Font("Tahoma", Font.BOLD, 50);
+        //Sets the text area font to be equal to our Font object
+        firstTextArea.setFont(font2);
+        //Block the user from editing the textArea
+        firstTextArea.setEditable(false);
+        firstTextArea.setForeground(Color.white);
+        firstTextArea.setBackground(Color.DARK_GRAY);
+        //=================LAUNCH TEXT AREA END ======================
 
+
+        LaunchScene(launchTextarea, launchSceneButton);
+        firstLaunchPanel(firstTextArea, firstChoiceButton, secondChoiceButton, thirdChoiceButton);
 
 
         //---------JFrame------------
         //Values that set the final int value for the frame width and height
-        final int FRAME_WIDTH = 900;
-        final int FRAME_HEIGHT = 700;
+        int FRAME_WIDTH = 1400;
+        int FRAME_HEIGHT = 700;
         //Values that set the final value for the frame location (x and y)
-        final int FRAME_LOC_X = 350;
-        final int FRAME_LOC_Y = 230;
+        int FRAME_LOC_X = 250;
+        int FRAME_LOC_Y = 130;
         //Sets the frame to be visible
         setVisible(true);
         //This regulates the size of the frame
@@ -66,15 +72,38 @@ public class MyGui extends JFrame {
         setLocation(FRAME_LOC_X,FRAME_LOC_Y);
         //Registers 'Exit upon closing' as close operation
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(launchGamePanel);
 
 
-        //---------JPane------------
-        //This is the middle pane of the frame
-        Container cPane = getContentPane();
-        //This Changes the background color of the pane
-        cPane.setBackground(Color.red);
-        //Adds the textArea to the pane.
-        cPane.add(textArea);
+        //What happens when the button is pressed
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //This sets the current panel(scene) (launch game panel) to false and then adds the
+                //next panel(scene)
+                launchGamePanel.setVisible(false);
+                add(firstScenePanel);
+            }
+        };
+        launchSceneButton.addActionListener(listener);
 
     }
+
+   public void LaunchScene(JTextArea launchTextarea, JButton launchSceneButton)
+   {
+       launchGamePanel.setBackground(Color.darkGray);
+       launchGamePanel.add(launchTextarea);
+       launchGamePanel.add(launchSceneButton);
+   }
+
+   public void firstLaunchPanel(JTextArea firstTextArea, JButton firstChoiceButton, JButton secondChoiceButton, JButton thirdChoiceButton)
+   {
+       firstScenePanel.setBackground(Color.DARK_GRAY);
+       firstScenePanel.add(firstTextArea);
+       firstScenePanel.add(firstChoiceButton);
+       firstScenePanel.add(secondChoiceButton);
+       firstScenePanel.add(thirdChoiceButton);
+
+   }
 }
