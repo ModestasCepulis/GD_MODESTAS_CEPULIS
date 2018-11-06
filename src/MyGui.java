8c575e2540c1f7e2bcbdfd2132465c4f863980c6
Modestas Cepulis <Modestas.Cepulis@students.ittralee.ie>
2018-11-06 10:57:59 +0000

    Trying to do the GUI, was able just to make out the frame of the GUI  06/11/2018

---
+++ src/MyGui.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyGui {
+
+
+    public static void main(String[] args) {
+
+        new MyGui();
+
+    }
+
+    public MyGui()
+    {
+
+        //Creates the Jframe window
+        JFrame frame = new JFrame("Window test");
+        //Sets the frame to be visible
+        frame.setVisible(true);
+        //This regulates the size of the frame
+        frame.setSize(200,300);
+        //This changes the background color of the frame
+
+    }
+}
