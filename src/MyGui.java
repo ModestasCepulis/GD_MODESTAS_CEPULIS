1f43efb741939057f64c1f13140cd57b9f358661
Modestas Cepulis <modestas.cepulis@students.ittralee.ie>
2018-11-06 12:28:05 +0000

    Managed to create a whole JFrame and pane inside of it. Inside of that pane I created a label that holds an object called textArea which creates the text.
    I managed to change the pane background color to Dark Gray, and change the font, type and color of the textArea text.
    
    My next attempt is going to be to create a button 'Play' that allows the user to move on to the next 'screen' and actually start playing the game.
    
    And I will try to position the text to the correct position, preferably the center as it is touching the left screen 'wall' at the moment.
    
    The button 'Play' should set the existing Gui to .setVisible(false) and should set the next labels to .setVisible(true) at least I think that it should do this not sure yet cuz I haven't actually tried it.
    
    Modestas Cepulis T00199237
    06/11/2018

--- src/MyGui.java
+++ src/MyGui.java
@@ -1,7 +1,10 @@
+import org.w3c.dom.Text;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class MyGui {
+//JFrame extends MyGui which means that we can use the public methods that are in JFrame class
+public class MyGui extends JFrame {
 
 
     public static void main(String[] args) {
@@ -13,13 +16,65 @@ public class MyGui {
     public MyGui()
     {
 
-        //Creates the Jframe window
-        JFrame frame = new JFrame("Window test");
+
+
+
+
+
+        //---------First Launch Panel------------
+
+        //Creates a text area
+        JTextArea textArea = new JTextArea();
+        //Sets the text area text to be
+        textArea.setText("\n\n\nBegin your Adventure!");
+        //Creates a Font object which sets the font parameters
+        Font font = new Font("Segoe Script", Font.BOLD, 50);
+        //Sets the text area font to be equal to our Font object
+        textArea.setFont(font);
+        //Block the user from editing the textArea
+        textArea.setEditable(false);
+        textArea.setForeground(Color.white);
+        textArea.setBackground(Color.DARK_GRAY);
+
+        /*
+        //Creates a label area (Like a title)
+        JLabel label = new JLabel();
+        label.setText("Begin your Adventure!");
+        */
+
+        //This creates a panel, (More like a holder) for the objects that you
+        //want to keep in this specific container and which ones you want to show/add
+        JPanel panel = new JPanel();
+        panel.add(textArea);
+        //Adds specified objects to the panel
+        /*panel.add(label);*/
+
+
+
+        //---------JFrame------------
+        //Values that set the final int value for the frame width and height
+        final int FRAME_WIDTH = 900;
+        final int FRAME_HEIGHT = 700;
+        //Values that set the final value for the frame location (x and y)
+        final int FRAME_LOC_X = 350;
+        final int FRAME_LOC_Y = 230;
         //Sets the frame to be visible
-        frame.setVisible(true);
+        setVisible(true);
         //This regulates the size of the frame
-        frame.setSize(200,300);
-        //This changes the background color of the frame
+        setSize(FRAME_WIDTH,FRAME_HEIGHT);
+        //This sets the window in the spiecified location
+        setLocation(FRAME_LOC_X,FRAME_LOC_Y);
+        //Registers 'Exit upon closing' as close operation
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        //---------JPane------------
+        //This is the middle pane of the frame
+        Container cPane = getContentPane();
+        //This Changes the background color of the pane
+        cPane.setBackground(Color.red);
+        //Adds the textArea to the pane.
+        cPane.add(textArea);
 
     }
 }
